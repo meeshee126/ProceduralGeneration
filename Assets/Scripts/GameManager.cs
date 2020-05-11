e90748b2fa7c7561f1bce3f572daada0fac73247
meeshee126 <47327880+meeshee126@users.noreply.github.com>
2020-05-11 16:29:40 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Slider slider;
+    public Text spacing;
+    public ObjectGenerator objectGenerator;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        spacing.text = slider.value.ToString();
+        objectGenerator.spacing = slider.value;
+    }
+
+    public void ButtonGenerate()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Object");
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            Destroy(objects[i]);
+        }
+
+        objectGenerator.Start();
+    }
+}
